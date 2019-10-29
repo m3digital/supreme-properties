79ec3417a0ef580e02c534364dff9434732bf96e
M³ Digital <mattguy19@gmail.com>
2019-10-29 09:26:48 -0400

    a few href changes

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,9 +14,9 @@ $subject = "Website Contact Form:  $name";
 
 
 // Create the email and send the message
-$to = 'test@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'mattguy19@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nFirst Name: $name\n\nEmail Address: $email_address\n\nMessage:\n\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@supremepropertiesri.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$subject,$email_body,$headers);
 echo json_encode(array('success'=>'true'));
