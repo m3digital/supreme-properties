c72c19fe4275cac6f6a266673aac02b1d4449fd0
Matt Gennings <mattguy19@gmail.com>
2020-12-13 11:54:22 -0500

    defect(textify)doubled load speed on wow animated text

--- js/textify.js
+++ js/textify.js
@@ -31,7 +31,7 @@ textify = function(element,method,delay) {
         count = 0;
         $(element).find('span').each(function () {
             if(method == 'fade'){
-                $(this).delay(0 + in_speed * count).animate({ opacity: '1' }, 1000);
+                $(this).delay(0 + in_speed * count).animate({ opacity: '1' }, 500);
             } else if(method == 'bounce'){
                 $(this).delay(0 + in_speed * count).animate({ opacity: '1','top':'-4px'}, 220,'easeOutCubic');
                 $(this).delay(0 + in_speed * count/4).animate({ opacity: '1','top':'0px'}, 220);
